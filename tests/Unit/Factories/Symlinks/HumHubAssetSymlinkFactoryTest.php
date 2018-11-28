80f2b87b0b96c07a1e154fd96e40485eeb09861e
Pierre Pivorius <ppi@move-elevator.de>
2018-11-28 17:27:09 +0100

    add test for assert collect factory

---
+++ tests/Unit/Factories/Symlinks/HumHubAssetSymlinkFactoryTest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace MoveElevator\Composer\Tests\Unit\Factories\Symlinks;
+
+use Composer\Config;
+use MoveElevator\Composer\Factories\Symlinks\HumHubAssetSymlinkFactory;
+use MoveElevator\Composer\Tests\Fixture\Models\InstallationConfigurationFixture;
+use PHPUnit\Framework\TestCase;
+
+class HumHubAssetSymlinkFactoryTest extends TestCase
+{
+    use InstallationConfigurationFixture;
+
+    public function testCreateCollectionWith7Entries()
+    {
+        $vendorDir = __DIR__ . '/../../../Fixture/VendorFolderStructure';
+
+        $configMock = $this->getMockBuilder(Config::class)->disableOriginalConstructor()->getMock();
+        $configMock->expects($this->any())->method('get')->willReturnMap([
+            ['vendor-dir', 0, $vendorDir],
+            ['vendor-dir', Config::RELATIVE_PATHS, 'VendorFolderStructure']
+        ]);
+
+        $installationConfiguration = $this->getInstallationConfiguration($configMock);
+
+        $collection = HumHubAssetSymlinkFactory::create($installationConfiguration);
+
+        $this->assertEquals(3, $collection->count());
+    }
+}
